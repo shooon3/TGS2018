3c9da124824ff50c0b2ddfc5301c9201ea04d9df
DESKTOP-28F38VP\user <nkc.kohei.suzuki@gmail.com>
2018-04-24 15:00:19 +0900

    連鎖反応

---
+++ Assets/suzuki/Hole.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public int myNumber;    //管理番号
+    [SerializeField]
+    bool b;                 //
+    [SerializeField]
+    CapsuleCollider col;    //判定
+    [SerializeField]
+    Material nextColor;     //変更色(仮)
+
+    HoleManager manager;    //マネジメントスクリプト
+
+	void Start ()
+    {
+        //初期値
+        b = false;
+        col = GetComponent<CapsuleCollider>();
+        manager = transform.root.GetComponent<HoleManager>();
+
+        //col.center = new Vector3(0, 12, 0);
+	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="f"></param>
+    public void Invasion(float f)
+    {
+        b = true;
+        StartCoroutine(Chain(f));
+    }
+    
+    void OnTriggerEnter(Collider collider)
+    {
+        if (b == false && collider.tag == "Player")
+        {
+            b = true;
+
+            //HoleManager伝える
+            manager.Unification(myNumber);
+        }
+    }
+
+    IEnumerator Chain(float f)
+    {
+        yield return new WaitForSeconds(f);
+
+        //色変え(仮)
+        gameObject.GetComponent<Renderer>().material = nextColor;
+    }
+}
---
+++ Assets/suzuki/HoleManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleManager : MonoBehaviour
+{
+    Hole[] hole;
+    
+	void Start ()
+    {
+        //子オブジェクトの穴を取得
+        hole = new Hole[transform.childCount];
+        for (int i = 0; i < hole.Length; i++)
+        {
+            hole[i] = transform.GetChild(i).GetComponent<Hole>();
+            hole[i].myNumber = i;   //管理番号を振る
+        }
+	}
+
+    public void Unification(int num)
+    {
+        //同レーンの他の穴も
+        float f1 = 0, f2 = 0.5f;
+        for (int i = num; i < hole.Length; i++)
+        {
+            //時間差を渡す(右方向)
+            hole[i].Invasion(f1);
+            f1 += f2;
+        }
+        f1 = f2;
+        for (int i = num - 1; i >= 0; i--)
+        {
+            //時間差を渡す(左方向)
+            hole[i].Invasion(f1);
+            f1 += f2;
+        }
+    }
+}
