e091697c67e0ceb68dba920e61f791861fa62f48
DESKTOP-28F38VP\user <nkc.kohei.suzuki@gmail.com>
2018-05-01 15:03:01 +0900

    耐久値を数値化

--- Assets/suzuki/Hole.cs
+++ Assets/suzuki/Hole.cs
@@ -4,25 +4,52 @@ using UnityEngine;
 
 public class Hole : MonoBehaviour
 {
-    public int myNumber;    //管理番号
     [SerializeField]
-    bool b;                 //
+    bool invincible;        //無敵
+    [SerializeField]
+    bool infection;         //感染
+    [SerializeField]
+    int HP;
+    [SerializeField]
+    int DecreaseHP;
+    [SerializeField]
+    float invincibleTime;   //無敵時間
     [SerializeField]
     CapsuleCollider col;    //判定
     [SerializeField]
     Material nextColor;     //変更色(仮)
+    Material defaultColor;  //変更色(仮)
 
     HoleManager manager;    //マネジメントスクリプト
 
 	void Start ()
     {
         //初期値
-        b = false;
-        col = GetComponent<CapsuleCollider>();
+        invincible = false;
+        infection = false;
+        HP = 1000;
+        DecreaseHP = 50;
+        invincibleTime = 0.5f;
+        defaultColor = gameObject.GetComponent<Renderer>().material;
         manager = transform.root.GetComponent<HoleManager>();
+    }
 
-        //col.center = new Vector3(0, 12, 0);
-	}
+    /// <summary>
+    /// 判定範囲調整
+    /// </summary>
+    /// <param name="f"></param>
+    public void SetColider(float f)
+    {
+        col = GetComponent<CapsuleCollider>();
+        col.radius = f;
+        col.height = col.radius * 100 + 22;
+        col.center = new Vector3(0, 12 + col.radius * 10, 0);
+    }
+
+    public void Flash(float f)
+    {
+        StartCoroutine(FlashCoroutine(f));
+    }
 
     /// <summary>
     /// 
@@ -30,19 +57,66 @@ public class Hole : MonoBehaviour
     /// <param name="f"></param>
     public void Invasion(float f)
     {
-        b = true;
+        infection = true;
         StartCoroutine(Chain(f));
     }
-    
-    void OnTriggerEnter(Collider collider)
-    {
-        if (b == false && collider.tag == "Player")
-        {
-            b = true;
 
-            //HoleManager伝える
-            manager.Unification(myNumber);
+    //void OnTriggerEnter(Collider collider)
+    //{
+    //    if (b == false && collider.tag == "Player")
+    //    {
+    //        b = true;
+
+    //        //HoleManager伝える
+    //        manager.Unification(gameObject.GetComponent<Hole>());
+    //    }
+    //}
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (infection == false && collider.tag == "Player")
+        {
+            if (invincible == false && HP > 0)
+            {
+                //耐久値減少
+                HP -= DecreaseHP;
+
+                if (HP <= 0)//感染
+                {
+                    infection = true;
+
+                    //感染をHoleManagerに伝える
+                    manager.Unification(gameObject.GetComponent<Hole>(), HP);
+                }
+                else//通常
+                {
+                    //無敵時間
+                    StartCoroutine(InvincibleTime());
+
+                    //感染をHoleManagerに伝える
+                    manager.Unification(gameObject.GetComponent<Hole>(), HP);
+                }
+            }
         }
+    }
+
+    IEnumerator FlashCoroutine(float f)
+    {
+        yield return new WaitForSeconds(f);
+        gameObject.GetComponent<Renderer>().material = nextColor;
+        yield return new WaitForSeconds(0.1f);
+        gameObject.GetComponent<Renderer>().material = defaultColor;
+    }
+
+    /// <summary>
+    /// 無敵時間(invincibleTime秒ダメージを受けない)
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator InvincibleTime()
+    {
+        invincible = true;
+        yield return new WaitForSeconds(invincibleTime);
+        invincible = false;
     }
 
     IEnumerator Chain(float f)
--- Assets/suzuki/HoleManager.cs
+++ Assets/suzuki/HoleManager.cs
@@ -5,34 +5,65 @@ using UnityEngine;
 public class HoleManager : MonoBehaviour
 {
     Hole[] hole;
+    [SerializeField]
+    float TimeLag;
+    [SerializeField]
+    float radius;
     
 	void Start ()
     {
+        
+
         //子オブジェクトの穴を取得
         hole = new Hole[transform.childCount];
         for (int i = 0; i < hole.Length; i++)
         {
             hole[i] = transform.GetChild(i).GetComponent<Hole>();
-            hole[i].myNumber = i;   //管理番号を振る
+            hole[i].SetColider(radius);
         }
 	}
 
-    public void Unification(int num)
+    public void Unification(Hole obj_hole, int hp)
     {
+        int num = 0;
+        while (num < hole.Length)
+        {
+            //何個目の穴か検索
+            if (obj_hole == hole[num])
+            {
+                break;
+            }
+            num++;
+        }
+
         //同レーンの他の穴も
-        float f1 = 0, f2 = 0.5f;
+        float f = 0;
         for (int i = num; i < hole.Length; i++)
         {
             //時間差を渡す(右方向)
-            hole[i].Invasion(f1);
-            f1 += f2;
+            if (hp > 0)
+            {
+                hole[i].Flash(f);
+            }
+            else
+            {
+                hole[i].Invasion(f);
+            }
+            f += TimeLag;
         }
-        f1 = f2;
+        f = TimeLag;
         for (int i = num - 1; i >= 0; i--)
         {
             //時間差を渡す(左方向)
-            hole[i].Invasion(f1);
-            f1 += f2;
+            if (hp > 0)
+            {
+                hole[i].Flash(f);
+            }
+            else
+            {
+                hole[i].Invasion(f);
+            }
+            f += TimeLag;
         }
     }
 }
