1cbc9d4cc675edbe151c1e9e6d2c45faae75bb0d
DESKTOP-28F38VP\user <nkc.ryo.shiratori@gmail.com>
2018-05-01 15:01:08 +0900

    味方AI_1.3

--- Assets/Shiratori_Work/Script/SerchHoles_1.cs
+++ Assets/Shiratori_Work/Script/SerchHoles_1.cs
@@ -5,10 +5,8 @@ using UnityEngine.AI;
 
 public class SerchHoles_1 : MonoBehaviour
 {
-    [SerializeField, Header("ホール 1")]
-    private GameObject Hole_1;
-    [SerializeField, Header("ホール 2")]
-    private GameObject Hole_2;
+    [SerializeField, Header("穴")]
+    private GameObject nearHole;
 
     NavMeshAgent agent;
 
@@ -16,13 +14,38 @@ public class SerchHoles_1 : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        nearHole = serchTag(gameObject, "Hole");
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = Hole_1.transform.position;
+        agent.destination = nearHole.transform.position;
+    }
 
-        agent.destination = Hole_2.transform.position;
+    GameObject serchTag(GameObject nearHole, string tagName)
+    {
+        float posDis = 0;  //一時変数
+        float nearDis = 0; //最も近いオブジェクトの距離
+
+        GameObject targetObj = null;
+
+        //タグ指定されたオブジェクトを配列で取得
+        foreach (GameObject obs in GameObject.FindGameObjectsWithTag(tagName))
+        {
+            //自身と取得したオブジェクトの距離を取得
+            posDis = Vector3.Distance(obs.transform.position, nearHole.transform.position);
+
+            //オブジェクトの距離が近いか、距離0であればオブジェクト名を取得
+            //一時変数に距離を格納
+            if (nearDis == 0 || nearDis > posDis)
+            {
+                nearDis = posDis;
+                //nearObjName = obs.name;
+                targetObj = obs;
+            }
+        }
+        return targetObj;
     }
 }
