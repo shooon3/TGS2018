bd96d9806125664b40b0b16902680794b3e9b347
DESKTOP-28F38VP\user <nkc.yuki.saitou@gmail.com>
2018-05-07 09:49:49 +0900

    パンプキンが増える、生成される、放出される

---
+++ Assets/Editor/NamedArrayDrawer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 配列の要素(Element)の名前を変える
+/// https://qiita.com/miyumiyu/items/eee6cfbdc6078270450c
+/// </summary>
+[CustomPropertyDrawer(typeof(NamedArrayAttribute))]
+public class NamedArrayDrawer : PropertyDrawer
+{
+
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+    {
+        try
+        {
+            int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+            EditorGUI.PropertyField(rect, property, new GUIContent(((NamedArrayAttribute)attribute).names[pos]));
+
+        }
+        catch
+        {
+            EditorGUI.PropertyField(rect, property, label);
+        }
+    }
+}
