9a0e8fd5e1f5f8217ddfd97a532d4e5c23d73eba
DESKTOP-28F38VP\user <nkc.ryo.shiratori@gmail.com>
2018-05-11 13:34:39 +0900

    味方AI_1.4

---
+++ Assets/Shiratori_Work/Script/VirusDestoroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusDestoroy : MonoBehaviour {
+
+    [SerializeField, Header("消滅するまでの時間")]
+    private float dethTime;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //プレイヤータグを検知
+        if (other.gameObject.tag == "Player")
+        {
+            //パンプ菌を削除
+            Destroy(other.gameObject, dethTime);
+        }
+    }
+}
