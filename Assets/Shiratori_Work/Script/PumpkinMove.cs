67123d16cf0d70c8fd17e5ba934f5fa6a4e63038
DESKTOP-28F38VP\user <nkc.ryo.shiratori@gmail.com>
2018-05-11 13:44:24 +0900

    畑に入ったら消えます、親の出現位置から近い穴を見つけてそこにパンプ菌が向かっていきます

---
+++ Assets/Shiratori_Work/Script/PumpkinMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PumpkinMove : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+    //SerchHolesスクリプトが取得した位置を取得するための変数
+    SerchHoles serchHoles;
+
+    //近い穴の位置を格納する変数
+    GameObject nerHole;
+
+    //親の位置を取得する変数
+    Vector3 parentPos;
+
+	// Use this for initialization
+	void Start () {
+
+        agent = GetComponent<NavMeshAgent>();
+
+        //SerchHolesスクリプトを取得
+        serchHoles = gameObject.GetComponentInParent<SerchHoles>();
+
+        //親のオブジェクトの位置を取得
+        parentPos = transform.parent.transform.position;
+
+        //親のオブジェクトとHoleタグを探す
+        nerHole = serchHoles.serchTag(parentPos, "Hole");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        agent.destination = nerHole.transform.position;
+    }
+}
