a2b948b8fb22ff653411cb84040f9b8a019be98c
DESKTOP-28F38VP\user <nkc.ryo.shiratori@gmail.com>
2018-06-21 09:32:25 +0900

    ステージシーン追加

---
+++ Assets/Shiratori_Work/Script/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    [Header("回転速度")]
+    public float speed;
+
+	// Use this for initialization
+	void Update ()
+    {
+        transform.Rotate(new Vector3(0, speed, 0) * Time.deltaTime, Space.World);
+    }
+}
