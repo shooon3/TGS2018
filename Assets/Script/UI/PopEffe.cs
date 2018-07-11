51204a5bf94032c870745ab2a12e8381b01c1593
DESKTOP-28F38VP\user <nkc.shota.tanikawa@gmail.com>
2018-07-11 15:27:45 +0900

    仮エフェクト追加

---
+++ Assets/Script/UI/PopEffe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopEffe : MonoBehaviour
+{
+    public GameObject a;
+    public GameObject b;
+    public GameObject c;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(Pop);
+        a.SetActive(false);
+        b.SetActive(false);
+        c.SetActive(false);
+    }
+	
+    public IEnumerator Pop()
+    {
+        yield return new WaitForSeconds(0.5f);
+        a.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        b.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        c.SetActive(false);
+        yield break;
+
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Push");
+        }
+	}
+}
