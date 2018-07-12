ef5ed93d785cb6c0a0f281856e98fb188088d333
DESKTOP-28F38VP\user <nkc.yuki.saitou@gmail.com>
2018-07-12 14:58:43 +0900

    画像差し替え、HPバー実装

---
+++ Assets/Script/UI/LookSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookSprite : MonoBehaviour {
+
+    GameObject front;
+
+    // Use this for initialization
+    void Start () {
+        front = Camera.main.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        transform.LookAt(front.transform.position);
+	}
+}
